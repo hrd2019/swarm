803b00c193b2bd84797ebfdb9eb13f8edad85f5e
createfu <hw2102@outlook.com>
2020-06-30 17:19:04 +0800

    uproad

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use std::fs::File;
+use std::io::Read;
+use toml::value::Array;
+use serde_derive::Deserialize;
+
+#[derive(Deserialize)]
+#[derive(Debug)]
+pub struct Config {
+    pub hosts: Array,
+}
+
+pub fn parse_config() -> Config {
+    let file_path = "config/config.toml";
+    let mut file = match File::open(file_path) {
+        Ok(f) => f,
+        Err(e) => panic!("open file {} failed.\n err:{}\n", file_path, e),
+    };
+
+    let mut str_buffer = String::new();
+    match file.read_to_string(&mut str_buffer) {
+        Ok(s) => s,
+        Err(e) => panic!("read file failed: {}", e),
+    };
+
+    let config: Config = toml::from_str(&str_buffer).unwrap();
+    config
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
     println!("Hello, world!");
+
+    let config = swarm::parse_config();
+    println!("{:#?}", config);
 }
